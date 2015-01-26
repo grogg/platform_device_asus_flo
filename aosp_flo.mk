6ddf893c92434aa6d4ac5c57112504d0e5ba916a
Joshua Lee <lee.joshwa@gmail.com>
2015-01-26 14:34:24 -0700

    modify for gapps inclusion

--- aosp_flo.mk
+++ aosp_flo.mk
@@ -26,7 +26,7 @@ PRODUCT_DEVICE := flo
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := ASUS
-PRODUCT_RESTRICT_VENDOR_FILES := true
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/asus/flo/device.mk)
